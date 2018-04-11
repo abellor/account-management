31fa7289862630e576034c5ed9418460c0c2422e
Camilo Bello <cbello@velocitypartners.net>
2018-04-11 06:35:30 -0500

    Migrating UI to React

---
+++ server/routes/api.js
@@ -0,0 +1,28 @@
+let express = require('express')
+let router = express.Router()
+
+module.exports = (app, db) => {
+    
+    router.use(function timeLog (req, res, next) {
+       console.log('Time: ', Date.now())
+       next()
+    })
+
+    router.post('/requirement', (req, res) => {
+        db.collection('quotes').save(req.body, (err, result) => {
+            if (err) return console.log(err)
+      
+            console.log('saved to database')
+            res.redirect('/')
+        })
+    })
+
+    app.get('/requirement', (req, res) =>{
+        db.collection('quotes').find().toArray((err, result) => {
+            if (err) return console.log(err)
+            res.send({quotes: result})
+        })
+    });
+    
+    return router
+}
--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-const database      = require('./../conf/db')
+const database      = require('./conf/db')
 const path          = require("path");
 const express       = require("express");
 const bodyParser    = require("body-parser");
@@ -16,7 +16,7 @@ MongoClient.connect(database.url, (err, client) => {
     console.log("Connected to the mongoDB !");
     let db = client.db('accounts');
     
-    const api = require("./../routes/api")(app, db);
+    const api = require("./routes/api")(app, db);
     app.use('/api', api);
 
     app.listen(port, () => {
@@ -27,13 +27,3 @@ MongoClient.connect(database.url, (err, client) => {
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
-
-app.get('/', (req, res) =>{
-    db.collection('quotes').find().toArray((err, result) => {
-        if (err) return console.log(err)
-        // renders index.ejs
-        res.render('index.ejs', {quotes: result})
-    })
-});
-
-
